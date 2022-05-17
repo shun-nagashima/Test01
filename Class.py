a5198f233d18a4529230c3755a2f778a5cf59fb0
shun-nagashima <lukaluka0030@gmail.com>
2022-05-17 14:06:28 +0900

    追加

---
+++ Class.py
@@ -0,0 +1,11 @@
+class Student:
+    def avg(math,english): 
+         print((math+english)/2)
+
+a001 = Student
+a001.name="sato"
+print(a001.name)
+
+a002 = Student()
+a002.name="tanaka"
+print(a002.name)
